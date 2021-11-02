ab5847fec20ff746a8c7cf984c3aa03c40369bb8
madison sorah <madisonsorah@gmail.com>
2021-11-02 08:57:57 -0400

    commit tree c6cd734045c9fba62ad4058cb1f3dc32dbc5666d

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code here
+const mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+} 
+
+const wrapAdjective = function(visual = "*") {
+    return function(single = "special") {
+     return `You are ${visual}${single}${visual}!`;
+    }
+}
+
+wrapAdjective("%")("a dedication programmer");
+
+const saturdayFun = function(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+    };
+
+saturdayFun();
